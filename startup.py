cee26f653808ffd195a58b5723b171a9534e5aef
maximombro <maximombro@gmail.com>
2019-11-10 15:27:08 -0500

    Added a file for starting the GPT-2M system

---
+++ startup.py
@@ -0,0 +1,12 @@
+# Runs the GPT-2M system inside of a text interface
+
+# Imports
+import generalUtilities as gu
+
+# Main Thread
+def main():
+    print("Welcome")
+
+# Execute Main Thread
+if __name__ == '__main__':
+    main()
