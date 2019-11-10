650fd4d4e331ca52b3b659f52d29e0aa48e811e3
maximombro <maximombro@gmail.com>
2019-11-10 15:30:43 -0500

    Added basic text main menu

--- startup.py
+++ startup.py
@@ -5,7 +5,23 @@ import generalUtilities as gu
 
 # Main Thread
 def main():
-    print("Welcome")
+    # Welcome message
+    print("\n<< Welcome to GPT-2M Main Control >>")
+
+    # Enter menu functions
+    options = ["Generation Menu"]
+    gu.textMenu("GPT-2M Main Menu", options, "Quit", mainMenuFunctions)
+
+    # Safe to exit message
+    print("\nGPT-2M has shutdown successfully.\nIt is now safe to close this window.")
+
+# Functions for the main menu
+def mainMenuFunctions(answer):
+    # Check which option was selected
+    if answer == "0":
+        # Open the generation menu
+        options = ["Option_To_Be_Done"]
+        gu.textMenu("Generation Menu", options, "Back to Main Menu", mainMenuFunctions)
 
 # Execute Main Thread
 if __name__ == '__main__':
